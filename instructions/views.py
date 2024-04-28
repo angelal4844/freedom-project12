2dea9b5eaa61f0c4e2c7c212783247d87a6f570c
Jessica <jEssicay10@nycstudents.net>
2024-04-27 23:36:58 -0400

    update repo

---
+++ instructions/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+
+def instructions_view(request):
+    html = """
+    <html>
+        <body>
+            <!-- HTML -->
+            <h1>Instructions</h1>
+            <h2>Use the AWSD keys to help you move the sprite</h2>
+            <h3>You will be entering the RPG adventure game with many different adventures.</h3>
+            <h3>Directions: The game consist of three main level</h3>
+            <ul>
+                <li>Restaurant</li>
+                <li>Software Engineering Program</li>
+            </ul>
+            <p>A: left</p>
+            <p>W: up</p>
+            <p>S: down</p>
+            <p>D: right</p>
+        </body>
+    </html>
+    """
+    return HttpResponse(html)
