01bfd108582ad6501f7902e9bafe173ea020f0fd
Jessica <jEssicay10@nycstudents.net>
2024-04-27 23:58:56 -0400

    update repo

--- instructions/views.py
+++ instructions/views.py
@@ -8,7 +8,7 @@ def instructions_view(request):
             <h1>Instructions</h1>
             <h2>Use the AWSD keys to help you move the sprite</h2>
             <h3>You will be entering the RPG adventure game with many different adventures.</h3>
-            <h3>Directions: The game consist of three main level</h3>
+            <h3>Directions: The game consist of one main level</h3>
             <ul>
                 <li>Restaurant</li>
                 <li>Software Engineering Program</li>
